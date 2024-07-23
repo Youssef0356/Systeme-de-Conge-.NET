41e617e7135a2925f0185615f3a939dae289200e
Youssef_Neji <137285575+Youssef0356@users.noreply.github.com>
2024-07-23 14:45:10 +0100

    Add files via upload

---
+++ ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿namespace testingdatabase.Models;
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public override string UserName { get; set; } = "";
+    public override string Email { get; set; } = "";
+
+    public int Sold { get; set; } = 22;
+}
---
+++ LeaveRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace testingdatabase.Models
+{
+
+    public class LeaveRequest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int Sold { get; set; } = 22;
+
+        [Required] 
+        public string UserId { get; set; }
+
+        [Required] 
+        public string Description { get; set; }
+
+        [Required] 
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required] 
+        public string Status { get; set; }
+
+
+    }
+}
